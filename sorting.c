12fff184723d72d74df0ebdc142c11093c760f14
Bhavika Choudhary <bhavikac16@gmail.com>
2022-07-27 23:29:31 +0530

    insertion sort

--- sorting.c
+++ sorting.c
@@ -24,15 +24,30 @@ void bubbleSort(int array[], int n)
         printf("%d ", array[c]);
 }
   
+//Insertion sort
+void insertionSort(int array[], int size) {
+    for (int step = 1; step < size; step++) {
+        int key = array[step];
+        int j = step - 1;
+
+        // Compare key with each element on the left of it until an element smaller than
+        // it is found.
+        // For descending order, change key<array[j] to key>array[j].
+        while (key < array[j] && j >= 0) {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = key;
+    }
+    printf("Sorted list in ascending order:\n");
+
+    for (int c = 0; c < size; c++)
+        printf("%d ", array[c]);
+}
 
 
 
-
-
- 
-
-
-
+  
 
 int main(){
  
@@ -53,13 +68,18 @@ int main(){
         
     
     switch(x){
+        case 1:
+            insertionSort(number,n);
+            break;
+
         
 
         case 3:
             bubbleSort(number, n);
             break;
 
-          
+           
+ 
    
     }
     return 0;
